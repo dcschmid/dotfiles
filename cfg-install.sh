828a0a376b13a5527ee92146f56d9526c3799d08
Daniel Schmid <daniel.schmid@libertymail.net>
2017-02-13 20:29:24 +0100

    Change config name

--- cfg-install.sh
+++ cfg-install.sh
@@ -1,14 +1,14 @@
 git clone --bare https://bitbucket.org/dschmid/dotfiles $HOME/.cfg
 
-alias config='/usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias configdot='/usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 mkdir -p .config-backup
-config checkout
+configdot checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    configdot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
-config checkout
-config config status.showUntrackedFiles no
+configdot checkout
+configdot config status.showUntrackedFiles no
