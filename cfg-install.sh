007957812d3c1d1ef12c6795b8c6f2eaad66ea84
DAniel Schmid <daniel@bsdminbi.fritz.box>
2019-05-18 10:26:52 +0200

    Correct install script

--- cfg-install.sh
+++ cfg-install.sh
@@ -1,10 +1,15 @@
-rm -f .zshrc
-rm -rf .config/tint2
-rm -rf .config/openbox
-ln -s ~/dotfiles/.zshrc .zshrc
-ln -s ~/dotfiles/.Xdefaults .Xdefaults
-ln -s ~/dotfiles/.vimrc .vimrc
-ln -s /home/daniel/dotfiles/.vim .vim
-ln -s /home/daniel/dotfiles/.ctags.d .ctags.d
-ln -s /home/daniel/dotfiles/.config/tint2 .config/tint2
-ln -s /home/daniel/dotfiles/.config/openbox .config/openbox
+mkdir -p ~/.config
+rm -f ~/.zshrc
+rm -f ~/.Xdefaults
+rm -f ~/.vimrc
+rm -rf ~/.vim
+rm -rf ~/.ctags.d
+rm -rf ~/.config/tint2
+rm -rf ~/.config/openbox
+ln -s /home/daniel/dotfiles/.zshrc ~/.zshrc
+ln -s /home/daniel/dotfiles/.Xdefaults ~/.Xdefaults
+ln -s /home/daniel/dotfiles/.vimrc ~/.vimrc
+ln -s /home/daniel/dotfiles/.vim ~/.vim
+ln -s /home/daniel/dotfiles/.ctags.d ~/.ctags.d
+ln -s /home/daniel/dotfiles/.config/tint2 ~/.config/tint2
+ln -s /home/daniel/dotfiles/.config/openbox ~/.config/openbox
